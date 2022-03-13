0d4416979b28adad133b0f52ddee709e867f2089
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-13 09:16:05 -0600

    separate file i/o into own class

---
+++ src/sketch_file.py
@@ -0,0 +1,25 @@
+import sys
+FREECADPATH = '/usr/lib/freecad-python3/lib'
+sys.path.append(FREECADPATH)
+import FreeCAD as App
+import Part
+
+
+class SketchFile:
+    def __init__(self):
+        self.doc = self.create_doc()
+        self.sketch = self.create_sketch()
+
+    def create_doc(self):
+        return App.newDocument("sketcher script")
+
+    def save_doc(self):
+        self.doc.recompute
+        self.doc.saveAs(u"/home/jakobedel/code/freecad-py/freecad_saves/sketcher_script.FCStd")
+        print("Sketch saved sucessfully")
+
+    def create_sketch(self):
+        return self.doc.addObject("Sketcher::SketchObject", "Sketch")
+
+    def add_line(self, v1, v2):
+        self.sketch.addGeometry(Part.LineSegment(v1, v2))
