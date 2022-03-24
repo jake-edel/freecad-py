3aa1125cff8fae617de46868b2b2a3eeae9e77b3
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-24 08:09:25 -0700

    generate part file from list of constraint names

--- src/part_generator.py
+++ src/part_generator.py
@@ -1,26 +1,32 @@
 import sys, json
-from constraint_id import ConstraintManager
-from constraint_mapper import ConstraintMapper
+from constraint_manager import ConstraintManager
+from constraint_finder import ConstraintFinder
 FREECADPATH = '/usr/lib/freecad-python3/lib'
 sys.path.append(FREECADPATH)
 import FreeCAD as App
 from pprint import pprint
 
-templateFile = './data/templates/part_template.FCStd'
+templateFile = './templates/part_template.FCStd'
 constraintFile = './data/constraints/rail01_constraints.json'
 
 template = ConstraintManager(templateFile)
 
-cm = ConstraintMapper(constraintFile)
+cf = ConstraintFinder(constraintFile)
 
 parts = ["bot-post", "top-rail", "top-post"]
 
-pprint(template.constraints)
-pprint(cm.collect_constraints("bot-post"))
+for part in parts:
+    driving_constrants = cf.find_constraints(part)
 
-driving_constrants = cm.collect_constraints("bot-post")
+    for constraint in driving_constrants:
+        # print("Constraint Name: " + constraint.split("_")[1])
+        # print("Constraint Type: " + driving_constrants[constraint]['type'])
+        # print("Constraint Value: " + driving_constrants[constraint]['value'])
+        # print("Template Value: " + template.constraints[constraint.split("_")[1]]['value'])
+        # print()
+        if driving_constrants[constraint]['type'].startswith("Distance"):
+            template.set_distance_constraint(constraint.split("_")[1], driving_constrants[constraint]['value'])
+        else:
+            template.set_angle_constraint(constraint.split("_")[1], driving_constrants[constraint]['value'])
 
-for constraint in driving_constrants:
-    print(constraint.split("_")[1]['value'])
-
-# template.set_constraint
+    template.save_doc('./data/parts/rail01_' + part)
