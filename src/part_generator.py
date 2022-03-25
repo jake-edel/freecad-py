feb52c0d26a7d762e8428309008457e2cef55b54
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-25 09:03:29 -0600

    refactor + flexible file names

--- src/part_generator.py
+++ src/part_generator.py
@@ -1,32 +1,24 @@
-import sys, json
 from constraint_manager import ConstraintManager
 from constraint_finder import ConstraintFinder
-FREECADPATH = '/usr/lib/freecad-python3/lib'
-sys.path.append(FREECADPATH)
-import FreeCAD as App
-from pprint import pprint
 
 templateFile = './templates/part_template.FCStd'
-constraintFile = './data/constraints/rail01_constraints.json'
-
 template = ConstraintManager(templateFile)
 
-cf = ConstraintFinder(constraintFile)
+rails = ['rail01', 'rail02', 'rail03']
 
-parts = ["bot-post", "top-rail", "top-post"]
+for rail in rails:
+    constraintFile = './data/constraints/' + rail + '_constraints.json'
+    cf = ConstraintFinder(constraintFile)
 
-for part in parts:
-    driving_constrants = cf.find_constraints(part)
+    parts = ["bot-post", "top-rail", "top-post"]
 
-    for constraint in driving_constrants:
-        # print("Constraint Name: " + constraint.split("_")[1])
-        # print("Constraint Type: " + driving_constrants[constraint]['type'])
-        # print("Constraint Value: " + driving_constrants[constraint]['value'])
-        # print("Template Value: " + template.constraints[constraint.split("_")[1]]['value'])
-        # print()
-        if driving_constrants[constraint]['type'].startswith("Distance"):
-            template.set_distance_constraint(constraint.split("_")[1], driving_constrants[constraint]['value'])
-        else:
-            template.set_angle_constraint(constraint.split("_")[1], driving_constrants[constraint]['value'])
+    for part in parts:
+        driving_constrants = cf.find_constraints(part)
 
-    template.save_doc('./data/parts/rail01_' + part)
+        for constraint in driving_constrants:
+            if driving_constrants[constraint]['type'].startswith("Distance"):
+                template.set_distance_constraint(constraint.split("_")[1], driving_constrants[constraint]['value'])
+            else:
+                template.set_angle_constraint(constraint.split("_")[1], driving_constrants[constraint]['value'])
+
+        template.save_doc('./data/parts/' + rail + '_' + part)
